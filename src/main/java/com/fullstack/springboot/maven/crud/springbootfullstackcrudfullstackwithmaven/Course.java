f775463b7306396a3d0e0359fabd968704b9c484
Dim Gr <gripeosd@yahoo.com>
2020-02-20 13:56:41 +0200

    Course class added

---
+++ src/main/java/com/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/Course.java
@@ -0,0 +1,12 @@
+package com.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven;
+
+public class Course {
+
+        private Long id;
+        private String username;
+        private String description;
+        //no arg constructor
+        //constructor with 3 args
+        //getters and setters
+        //hashcode and equals
+}
