9e6208c6a1e4cda4124eeedb7295b949afe10496
Dim Gr <gripeosd@yahoo.com>
2020-04-29 12:33:27 +0300

    Some additions for jar

--- src/main/java/com/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/SpringBootFullstackCrudFullStackWithMavenApplication.java
+++ src/main/java/com/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/SpringBootFullstackCrudFullStackWithMavenApplication.java
@@ -2,12 +2,13 @@ package com.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwith
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringBootFullstackCrudFullStackWithMavenApplication {
+public class SpringBootFullstackCrudFullStackWithMavenApplication   {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootFullstackCrudFullStackWithMavenApplication.class, args);
